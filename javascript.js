0894824aeace5f701f28425825f5960babf28b57
juiceman1014 <justintran1014@gmail.com>
2023-11-18 15:06:43 -0800

    added recursive approach of fibonacci sequence

--- javascript.js
+++ javascript.js
@@ -1,17 +1,31 @@
 //iterative approach
-function fibs(num){
-    //8
-    //index 0 = 0
-    //index 1 = 1
-    //index - 2 + index  - 1 = index
-    //populate myArray with that index
+function fibsIterative(num){
     let myArray = [];
     myArray[0] = 0;
     myArray[1] = 1;
+
     for(i = 2; i < num; i++){
         myArray[i] = myArray[i-2] + myArray[i-1];
     }
+
     return myArray;
 }
 
-console.log(fibs(8));
+console.log(fibsIterative(8));
+
+
+
+//recursive approach
+function fibsRec(num) {
+    if (num <= 0 || isNaN(num)) {
+      return "Please enter a valid number."
+    } else if (num === 1) {
+        return [0]
+    } else if (num === 2) {
+        return [0, 1]
+    } else if (num > 2) {
+        return [...fibsRec(num-1), fibsRec(num - 1)[num - 2] + fibsRec(num-1)[num - 3]]
+    }
+  }
+  
+  console.log(fibsRec(3));
