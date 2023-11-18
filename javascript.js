3a438aeca87d726f3f3de5262216312523e3ded7
juiceman1014 <justintran1014@gmail.com>
2023-11-18 12:45:11 -0800

    solved fibonacci sequence using an iterative approach

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+//iterative approach
+function fibs(num){
+    //8
+    //index 0 = 0
+    //index 1 = 1
+    //index - 2 + index  - 1 = index
+    //populate myArray with that index
+    let myArray = [];
+    myArray[0] = 0;
+    myArray[1] = 1;
+    for(i = 2; i < num; i++){
+        myArray[i] = myArray[i-2] + myArray[i-1];
+    }
+    return myArray;
+}
+
+console.log(fibs(8));
